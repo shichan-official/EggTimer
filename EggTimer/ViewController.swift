8085741e07f8f093a34c5cbaa9ba8eae60ed43ee
Siavash <svshjalali@gmail.com>
2020-05-19 01:27:29 +0900

    Progress bar and sound
    Added progress bar and completion sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,28 +1,38 @@
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]
-    var secondsRemaining = 0
+    var secondsPast = 0
+    var totalTime = 0
     var timer = Timer()
     @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var timerProgress: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
         
+        timerProgress.progress = 0.0
+        
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        secondsPast = 0
+        
+        titleText.text = "Cooking..."
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print(secondsRemaining)
-            secondsRemaining -= 1
+        if secondsPast < totalTime {
+            secondsPast += 1
         } else {
             titleText.text = "Done!"
+            AudioServicesPlayAlertSound(SystemSoundID(1322))
+            timer.invalidate()
         }
+        timerProgress.progress = Float(secondsPast)/Float(totalTime)
     }
 }
